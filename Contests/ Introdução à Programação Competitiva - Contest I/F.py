2711d2bd7494a77336a5e8453ae5c33aeab3f5a3
Bruno Dantas <oliveiradantas96@gmail.com>
2024-02-12 21:15:39 -0300

    Adicionando resposta da D.

--- Contests/ Introdução à Programação Competitiva - Contest I/F.py
+++ Contests/ Introdução à Programação Competitiva - Contest I/F.py
@@ -6,12 +6,4 @@ def min_digit_sum(n):
     for i in range(1, n):
         a = i
         b = n - i
-        digit_sum_a = digit_sum(a)
-        digit_sum_b = digit_sum(b)
-        min_sum = min(min_sum, digit_sum_a + digit_sum_b)
-    return min_sum
-
-
-if __name__== "__main__":
-    n = int(input())
-    print(min_digit_sum(n))
+        digit_sum_a = d
