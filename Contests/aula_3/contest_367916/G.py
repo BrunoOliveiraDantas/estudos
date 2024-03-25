ab771bd207c35303ea8dd2748cb720a2a9dda538
Bruno Dantas <oliveiradantas96@gmail.com>
2024-03-25 09:40:19 -0300

    Solucao da questão G

--- Contests/aula_3/contest_367916/G.py
+++ Contests/aula_3/contest_367916/G.py
@@ -1,23 +1,34 @@
-def sol():
+def solucao_correta():
+    str = input()
 
-    #tiles = input()
-    #tiles.replace("00", "01").replace("11", "10")
+    sample = '10' * 50001
+
+    print(min(sub(str, sample), sub(str, sample[1:])))
+
+def sub(s, t):
+    c = 0
+    for i in range(len(s)):
+        if s[i] != t[i]:
+            c += 1
+    return c
+
+def sol():
     digitos = [int(n) for n in input()]
     qtd_tiles = 0
     posicao_anterior = digitos[-1]
 
     for digito in reversed(digitos[:-1]):
-   
         if posicao_anterior == digito:
             qtd_tiles += 1
             if digito == 1:
                 posicao_anterior = 0
             if digito == 0:
                 posicao_anterior = 1
+        else:
+            posicao_anterior = digito
 
     return qtd_tiles
 
-   
-    
+
 if __name__== "__main__":
     print(sol())
